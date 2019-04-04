d182e52e5b2c41d2b0c46225c8123c1392f4b2bb
zhenghongfei <zhenghongfei@cmos.chinamobile.com>
2019-04-04 19:36:45 +0800

    相对路径

--- main/js/content.js
+++ main/js/content.js
@@ -14,11 +14,11 @@ function load() {
         var xiangfen = '';
         for (let i = 0; i < res.data.length; i++) {
           if (res.data[i].type === '护肤') {
-            hufu += '<div class="skin-item"><div class="skin-img"><a href="http://127.0.0.1:5500/buy/WEB-INF/jsp/buy.html?itemid=' + res.data[i].id + '"><img src="' + res.data[i].imgsrc + '"alt=""></a></div><div class="skin-desc"><div class="left">￥<span>' + res.data[i].price + '</span></div><div class="right">' + res.data[i].name + '&nbsp;&nbsp;已售' + res.data[i].volume + '</div></div></div>'
+            hufu += '<div class="skin-item"><div class="skin-img"><a href="../../../buy/WEB-INF/jsp/buy.html?itemid=' + res.data[i].id + '"><img src="' + res.data[i].imgsrc + '"alt=""></a></div><div class="skin-desc"><div class="left">￥<span>' + res.data[i].price + '</span></div><div class="right">' + res.data[i].name + '&nbsp;&nbsp;已售' + res.data[i].volume + '</div></div></div>'
           } else if (res.data[i].type === '美妆') {
-            meizhuang += '<div class="skin-item"><div class="skin-img"><a href="http://127.0.0.1:5500/buy/WEB-INF/jsp/buy.html?itemid=' + res.data[i].id + '"><img src="' + res.data[i].imgsrc + '" alt=""></a></div><div class="skin-desc"><div class="left">￥<span>' + res.data[i].price + '</span></div><div class="right">' + res.data[i].name + '&nbsp;&nbsp;已售' + res.data[i].volume + '</div></div></div>'
+            meizhuang += '<div class="skin-item"><div class="skin-img"><a href="../../../buy/WEB-INF/jsp/buy.html?itemid=' + res.data[i].id + '"><img src="' + res.data[i].imgsrc + '" alt=""></a></div><div class="skin-desc"><div class="left">￥<span>' + res.data[i].price + '</span></div><div class="right">' + res.data[i].name + '&nbsp;&nbsp;已售' + res.data[i].volume + '</div></div></div>'
           } else if (res.data[i].type === '香氛') {
-            xiangfen += '<div class="skin-item"><div class="skin-img"><a href="http://127.0.0.1:5500/buy/WEB-INF/jsp/buy.html?itemid=' + res.data[i].id + '"><img src="' + res.data[i].imgsrc + '" alt=""></a></div><div class="skin-desc"><div class="left">￥<span>' + res.data[i].price + '</span></div><div class="right">' + res.data[i].name + '&nbsp;&nbsp;已售' + res.data[i].volume + '</div></div></div>'
+            xiangfen += '<div class="skin-item"><div class="skin-img"><a href="../../../buy/WEB-INF/jsp/buy.html?itemid=' + res.data[i].id + '"><img src="' + res.data[i].imgsrc + '" alt=""></a></div><div class="skin-desc"><div class="left">￥<span>' + res.data[i].price + '</span></div><div class="right">' + res.data[i].name + '&nbsp;&nbsp;已售' + res.data[i].volume + '</div></div></div>'
           }
         }
         $('#hufu-content').html(hufu)
@@ -42,11 +42,11 @@ function list() {
       if (res.code === 200) {
         for (let i = 0; i < res.data.length; i++) {
           if (res.data[i].classify === '护肤') {
-            hufuList += '<li><a href="http://127.0.0.1:5500/buy/WEB-INF/jsp/buy.html?itemid="' + res.data[i].id + '>' + res.data[i].name + '</a></li>';
+            hufuList += '<li><a href="../../../buy/WEB-INF/jsp/buy.html?itemid="' + res.data[i].id + '>' + res.data[i].name + '</a></li>';
           } else if (res.data[i].classify === '美妆') {
-            meizhuangList += '<li><a href="http://127.0.0.1:5500/buy/WEB-INF/jsp/buy.html?itemid="' + res.data[i].id + '>' + res.data[i].name + '</a></li>';
+            meizhuangList += '<li><a href="../../../buy/WEB-INF/jsp/buy.html?itemid="' + res.data[i].id + '>' + res.data[i].name + '</a></li>';
           } else if (res.data[i].classify === '香氛') {
-            xiangfenList += '<li><a href="http://127.0.0.1:5500/buy/WEB-INF/jsp/buy.html?itemid="' + res.data[i].id + '>' + res.data[i].name + '</a></li>';
+            xiangfenList += '<li><a href="../../../buy/WEB-INF/jsp/buy.html?itemid="' + res.data[i].id + '>' + res.data[i].name + '</a></li>';
           }
         }
         $('#hufu-list').html(hufuList);
